7e4966dd37214c92b8dd071b63c6db87a368b43b
samkit.shah <samkit.shah@rakuten.com>
2019-06-11 23:00:22 +0900

    Intial commit

---
+++ src/main/java/com/wm/extendedwiremcok/ExtendedWiremcokApplication.java
@@ -0,0 +1,14 @@
+package com.wm.extendedwiremcok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendedWiremcokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtendedWiremcokApplication.class, args);
+		System.out.println("Hello World!!!");
+	}
+
+}
